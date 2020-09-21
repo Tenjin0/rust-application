4098c8c0eb66316757c79ad66551d9aed2923583
Tenji <patricepetit@hotmail.com>
2020-09-21 22:05:57 +0200

    feature/merge_two_list: WIP welcome to hell

--- merge/src/lib.rs
+++ merge/src/lib.rs
@@ -1,73 +1,61 @@
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
-
 
 impl ListNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
-
 }
 
+pub fn merge_two_lists(
+    l1: Option<Box<ListNode>>,
+    l2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+    let mut current_l1 = l1;
+    let mut current_l2 = l2;
 
+    if current_l1.is_none() && current_l2.is_none() {
+        return None;
+    } else if current_l1.is_none() && current_l2.is_some() {
+        return current_l2;
+    } else if current_l1.is_some() && current_l2.is_none() {
+        return current_l1;
+    }
 
-fn inner_merge_two_lists_rec(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-   
-  todo!();
-}
+    let mut result_head = Box::new(ListNode::new(0));
+    let mut current_head = &mut result_head;
+    let mut count = 0;
+	while current_l1.is_some() && current_l2.is_some() {
+		count = count + 1;
+        let mut current1_up = current_l1.take().unwrap();
+        let mut current2_up = current_l2.take().unwrap();
+        println!("{} current l1: {:?}", count, current1_up);
+        println!("{} current l2: {:?}", count, current2_up);
+        if current1_up.val <= current2_up.val {
+            current_l1 = current1_up.next.take();
+			current_head.next = Some(current1_up);
+			current_l2 = Some(current2_up);
+        } else {
+			current_l2 = current2_up.next.take();
+			current_head.next = Some(current2_up);
+			current_l1 = Some(current1_up);
 
+        }
+		println!("{} new current l1: {:?}", count, current_l1);
+		println!("{} new current l2: {:?}", count, current_l2);
+        current_head = current_head.next.as_mut().unwrap()
+    }
 
+	if current_l1.is_some() {
+		current_head.next = current_l1;
+	} else if current_l2.is_some() {
+		current_head.next = current_l2;
 
-pub fn create_min(p1: Option<Box<ListNode>>, p2: Option<Box<ListNode>>) -> ListNode {
-  let x: Box<ListNode> = p1.unwrap();
-  let y = p2.unwrap();
-  let start : ListNode;
-  if x.val < y.val {
-    let val = x.val;
-    start = ListNode::new(val)
-  } else {
-    let val = y.val;
-    start = ListNode::new(val)
-  }
-  return start;
-}
-
-pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-
-  if l1 == None && l2 == None {
-    return None
-  }
-  if l1 == None && l2 != None {
-    return l1
-  }
-
-  if l1 != None && l2 == None {
-    return l2
-  }
-
-  let mut result: Option<Box<ListNode>>;
-  let mut tail: &Option<Box<ListNode>>;
-
-  let p1: Option<Box<ListNode>> = l1;
-  let p2: Option<Box<ListNode>> = l2;
-  
-  // {
-    // let start = create_min(p1, p2);
-    // tail = &start.next;
-    // result = Some(Box::new(start));
-  // }
-
-  loop {
-    let mut next : Option<Box<ListNode>> = None;
-   
-    break;
-  }
-  todo!();
+	}
+	return result_head.next; 
+	
 }
--- merge/src/main.rs
+++ merge/src/main.rs
@@ -2,20 +2,20 @@ use merge::{ListNode, merge_two_lists};
 
 fn main() {
     
-    let mut node3 = ListNode::new(4);
-    let mut node2 = ListNode::new(2);
     let mut node1 = ListNode::new(1);
-
+    let mut node2 = ListNode::new(2);
+    let node3 = ListNode::new(4);
+    
     node2.next = Option::Some(Box::new(node3));
     node1.next = Option::Some(Box::new(node2));
     
-    let node6 = ListNode::new(4);
-    let mut node5 = ListNode::new(3);
     let mut node4 = ListNode::new(1);
+    let mut node5 = ListNode::new(3);
+    let node6 = ListNode::new(4);
 
     node5.next = Option::Some(Box::new(node6));
     node4.next = Option::Some(Box::new(node5));
 
-    merge_two_lists(Some(Box::new(node1)), Some(Box::new(node4)));
-
+    let result = merge_two_lists(Some(Box::new(node1)), Some(Box::new(node4)));
+    println!("{:?}", result)
 }
